41c5b4c56176829986fe8e2ab1987cda8b1eb824
alihan939 <50833895+alihan939@users.noreply.github.com>
2025-06-08 09:34:39 +0500

    config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+BOT_TOKEN = ''
+DB_PATH = "stats.db"
+OUTPUT_PATH = "/tmp/tiktok_video.mp4"
+MAX_QUEUE_SIZE = 10
+NUM_WORKERS = 3
+SUPPORT_URL = ''
+ADMIN_ID = 
+
+TIKTOK_URL_PATTERN = r'^https?://(www\.)?(vm\.)?tiktok\.com/.+'
